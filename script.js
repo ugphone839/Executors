8f93fc16ed73f6a259d338d3fd957aab6b883816
ugphone839 <testugphone3.6@gmail.com>
2025-10-31 11:41:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -89,10 +89,10 @@ document.addEventListener('DOMContentLoaded', () => {
             {
                 name: "Arceus X NEO",
                 logo: "https://sf-static.upanhlaylink.com/img/image_2025091341b69c6afa4e02709bf1ba299bdbafbd.jpg",
-                version: "6.695",
+                version: "6.696",
                 status: "Online",
                 links: [
-                    { text: "Global", url: "https://www.mediafire.com/file/llra8r3n7gi6n8a/Roblox_Arceus_X_NEO_1.9.0_-_FIXED.apk/file", className: "btn-download-main", statusText: "Global: Online" },
+                    { text: "Global", url: "https://www.mediafire.com/file/6iumqvioxh3oej8/Roblox_Arceus_X_NEO_1.9.1.apk/file", className: "btn-download-main", statusText: "Global: Online" },
                     { text: "VNG", url: "https://www.mediafire.com/file/glclfeebbmhzxeq/Roblox_-_VNG_Arceus_X_NEO_1.9.0.apk/file", className: "btn-download-alt", statusText: "VNG: Online" }
                 ]
             },
