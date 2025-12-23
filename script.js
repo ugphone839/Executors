28abe76c228b25a97e03e3986c78cc57beb18c6c
ugphone839 <testugphone3.6@gmail.com>
2025-12-24 05:36:02 +0700

    📦 Batch Update (3 changes) by ds_zeus.
    • 🔄 Uplink Codex v2.702
    • 🔄 Uplink Arceus X NEO v2.702
    • 🔄 Uplink Vega X v2.702

--- script.js
+++ script.js
@@ -114,10 +114,10 @@ document.addEventListener('DOMContentLoaded', () => {
             {
                 name: "Codex",
                 logo: "https://codex.lol/static/icons/icon.svg",
-                version: "2.701",
-                status: "Partial",
+                version: "2.702",
+                status: "Online",
                 links: [
-                    { text: "Global", url: "https://www.mediafire.com/file/8dxf37l1y6xtcct/Codex+v2.701.apk/file", className: "btn-download-main", statusText: "Global: Online" },
+                    { text: "Global", url: "https://www.mediafire.com/file/vtqurqxgsbejtoa/Codex+V2.702.632.apk/file", className: "btn-download-main", statusText: "Global: Online" },
                     { text: "VNG", url: "https://www.mediafire.com/file/8dxf37l1y6xtcct/Codex+v2.701.apk/file", className: "btn-download-alt", statusText: "VNG: Offline" }
                 ]
             },
@@ -162,7 +162,7 @@ document.addEventListener('DOMContentLoaded', () => {
             {
                 name: "Vega X",
                 logo: "https://vegax.gg/assets/img/V_logo_white.png",
-                version: "2.701",
+                version: "2.702",
                 status: "Online",
                 links: [
                     { text: "Download", url: "https://github.com/1f0yt/community/releases/download/Vegax/Vega.X.apk", className: "btn-download-main", statusText: "Status: Online" }
