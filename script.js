02e679b045385c482cd951a028267d127c3b4f9f
ugphone839 <testugphone3.6@gmail.com>
2026-01-13 11:28:53 +0700

    📦 Batch Update (1 changes) by ds_zeus.
    • 🔄 Uplink Vega X v2.703

--- script.js
+++ script.js
@@ -162,10 +162,10 @@ document.addEventListener('DOMContentLoaded', () => {
             {
                 name: "Vega X",
                 logo: "https://vegax.gg/assets/img/V_logo_white.png",
-                version: "2.702",
-                status: "Offline",
+                version: "2.703",
+                status: "Online",
                 links: [
-                    { text: "Download", url: "https://github.com/1f0yt/community/releases/download/Vegax/Vega.X.apk", className: "btn-download-main", statusText: "Status: Offline" }
+                    { text: "Download", url: "https://github.com/1f0yt/community/releases/download/Vegax/Vega.X.apk", className: "btn-download-main", statusText: "Status: Online" }
                 ]
             },
             {
