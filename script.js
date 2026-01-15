5c0f6d33142b803ddb5c714aa31870c0c83024a1
ugphone839 <testugphone3.6@gmail.com>
2026-01-15 21:11:53 +0700

    📦 Batch Update (1) by ds_zeus. • 🔄 Auto-Update Codex v2.703

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
             {
                 name: "Arceus X NEO", logo: "https://sf-static.upanhlaylink.com/img/image_2025091341b69c6afa4e02709bf1ba299bdbafbd.jpg", version: "2.703", status: "Online",
                 links: [
-                    { text: "Global", url: "https://www.mediafire.com/file/o6a2yhenrvhqzr5/Roblox_-_VNG_Arceus_X_NEO_1.9.8.apk/file", className: "btn-download-main", statusText: "Global: Online" },
+                    { text: "Global", url: "https://www.mediafire.com/file/8841v0l31pwj28e/Roblox_Arceus_X_NEO_1.9.8.apk/file", className: "btn-download-main", statusText: "Global: Online" },
                     { text: "VNG", url: "https://www.mediafire.com/file/jk5ilmfgau58xt8/Roblox_-_VNG_Arceus_X_NEO_1.9.7.apk/file", className: "btn-download-alt", statusText: "VNG: Online" }
                 ]
             },
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 name: "Codex", logo: "https://codex.lol/static/icons/icon.svg", version: "2.703", status: "Online",
                 links: [
                     { text: "Global", url: "https://gofile.io/d/UzbLud", className: "btn-download-main", statusText: "Global: Online" },
-                    { text: "VNG", url: "https://www.mediafire.com/file/8dxf37l1y6xtcct/Codex+v2.701.apk/file", className: "btn-download-alt", statusText: "VNG: Offline" }
+                    { text: "VNG", url: "https://www.mediafire.com/file/o6a2yhenrvhqzr5/Roblox_-_VNG_Arceus_X_NEO_1.9.8.apk/file", className: "btn-download-alt", statusText: "VNG: Online" }
                 ]
             },
             {
