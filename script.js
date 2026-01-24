fe170bce63bee4fd2c52e6689bb43ad50d50e1c7
ugphone839 <testugphone3.6@gmail.com>
2026-01-24 12:03:56 +0700

    📦 Update Ver version-8b8c34790f814a73 + Link for 1 clients (by ds_zeus.)

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 name: "Solara",
                 logo: "https://files.catbox.moe/gjzzjt.png",
                 version: "version-0f5223fb6e7e4955",
-                status: "Offline",
+                status: "Online",
                 links: [
-                    { text: "Download", url: "https://getsolara.dev/download/static/files/Solara.Dir.zip", className: "btn-download-main", statusText: "Status: Offline" }
+                    { text: "Download", url: "https://getsolara.dev/download/static/files/Solara.Dir.zip", className: "btn-download-main", statusText: "Status: Online" }
                 ]
             },
             {
@@ -45,10 +45,10 @@ document.addEventListener('DOMContentLoaded', () => {
             {
                 name: "Ronix PC",
                 logo: "https://wearedevs.net/images/ronix/ronix-animated.gif",
-                version: "version-1588a9c58c674e38",
-                status: "Offline",
+                version: "version-8b8c34790f814a73",
+                status: "Online",
                 links: [
-                    { text: "Download", url: "https://gofile.io/d/iVZKbO", className: "btn-download-main", statusText: "Status: Offline" }
+                    { text: "Download", url: "https://wrdcdn.net/r/154522/1769203129478/RonixInstaller_NEW.exe", className: "btn-download-main", statusText: "Status: Online" }
                 ]
             }
         ],
