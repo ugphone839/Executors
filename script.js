3414fdd5a0a1ceca10c2594144491eda010e28d1
ugphone839 <testugphone3.6@gmail.com>
2026-03-05 21:06:14 +0700

    Cập nhật từ Web Admin Panel

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ document.addEventListener('DOMContentLoaded', () => {
             {
                 name: "Velocity",
                 logo: "https://files.catbox.moe/tj6brv.png",
-                version: "version-df7528517c6849f7",
-                status: "Offline",
+                version: "version-d599f7fc52a8404c",
+                status: "Online",
                 links: [
-                    { text: "Download", url: "https://gofile.io/d/v12Uk8", className: "btn-download-main", statusText: "Status: Offline" }
+                    { text: "Download", url: "https://gofile.io/d/v12Uk8", className: "btn-download-main", statusText: "Status: Online" }
                 ]
             },
             {
@@ -27,19 +27,19 @@ document.addEventListener('DOMContentLoaded', () => {
             {
                 name: "Solara",
                 logo: "https://files.catbox.moe/gjzzjt.png",
-                version: "version-df7528517c6849f7",
-                status: "Offline",
+                version: "version-d599f7fc52a8404c",
+                status: "Online",
                 links: [
-                    { text: "Download", url: "https://getsolara.dev/download/static/files/Solara.Dir.zip", className: "btn-download-main", statusText: "Status: Offline" }
+                    { text: "Download", url: "https://getsolara.dev/download/static/files/Solara.Dir.zip", className: "btn-download-main", statusText: "Status: Online" }
                 ]
             },
             {
                 name: "Xeno",
                 logo: "https://www.xeno.onl/images/xeno.png",
-                version: "version-df7528517c6849f7",
-                status: "Offline",
+                version: "version-d599f7fc52a8404c",
+                status: "Online",
                 links: [
-                    { text: "Download", url: "https://xeno.onl/33054e6324afae35e63859ba1f6b492f/98000f2a/bdf367db6b50b0b2?hash=yRlw9F9FuzjA9u3rvEjh2drxPFZMzDdYivP0HkItk9uAfMhGn195mfrEoiMgzudAe3kZrBrkPQbOJ8bBsBpGbdoGpUPV08A4f0zm", className: "btn-download-main", statusText: "Status: Offline" }
+                    { text: "Download", url: "https://xeno.onl/33054e6324afae35e63859ba1f6b492f/98000f2a/bdf367db6b50b0b2?hash=b0NQUJlkKLXHHenQ4lJ2hQJqbQDAAWu1iUnh6Nchfvk2SFfvmuVX9Nds6B9ZSGHT", className: "btn-download-main", statusText: "Status: Online" }
                 ]
             },
             {
