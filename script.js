da03d37c13963e93a25d387285817592330bf351
ugphone839 <testugphone3.6@gmail.com>
2026-05-12 11:57:11 +0700

    Cập nhật từ Web Admin Panel

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ document.addEventListener('DOMContentLoaded', () => {
             {
                 name: "Delta",
                 logo: "https://sf-static.upanhlaylink.com/img/image_2025091320b780aa4e49c945bdddf569984a41b6.jpg",
-                version: "2.718",
+                version: "2.720",
                 status: "Online",
                 links: [
                     { text: "Global", url: "https://delta.filenetwork.vip/android.html", className: "btn-download-main", statusText: "Global: Online" },
